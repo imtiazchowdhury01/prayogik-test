37786739eba745c8d084f32b5e876dbcba47f2aa
imtiazchowdhury01 <imtiaz.xponent@gmail.com>
2025-09-09 16:16:16 +0600

    page

--- app/(course)/courses/category/[categorySlug]/[filter]/loading.tsx
+++ app/(course)/courses/category/[categorySlug]/[filter]/loading.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CategoryPageLoading = () => {
   return (
     <div>
-      <p>Loading category...</p>
+      <p>Loading category filter...</p>
     </div>
   );
 };
--- app/(course)/courses/category/[categorySlug]/loading.tsx
+++ app/(course)/courses/category/[categorySlug]/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CategoryPageLoading = () => {
     return (
         <div>
-            <p>Loading category...</p>
+            <p>Loading category slug...</p>
         </div>
     );
 };
